7bf8ddf183ae92e8dddf1b859cba4562d88aaaab
jibenwujie58 <cr0508@foxmail.com>
2021-07-21 16:08:53 +0800

    雪花算法

--- src/main/java/com/gmcc/aliface/controller/IdentityInfoQuery.java
+++ src/main/java/com/gmcc/aliface/controller/IdentityInfoQuery.java
@@ -8,6 +8,7 @@ import com.gmcc.aliface.response.FaceUser;
 import com.gmcc.aliface.response.Institution;
 import com.gmcc.aliface.response.JsonRootBean;
 import com.gmcc.aliface.response.Response;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,20 +22,22 @@ import java.util.Map;
 import java.util.TreeMap;
 
 @RestController
-@RequestMapping(value="/api/v1/aliUserReigstor")
+@RequestMapping(value = "/api/v1/aliUserReigstor")
 public class IdentityInfoQuery {
 
-    final String aliPubKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAl738H533ua5pVm9Y3/syjosi9y4BxprQjp4fYvqf5WpTtPPUW8BeSmdSRHT58QMJ+M68w7o65MAHTMXiX53C5YT0Kb4zRrdBb1EUm7WCvdTXS7NxOUTybhinW6UIjZTE2OhYbt9/wNc90tWBYMBaTWyPEzh6LlkOGG6yum/Cyy7Hy72Z9LT4F1va9GXK2SFFMhuWIxgIGxn78wKF7kLfpUXBUOIsZZJwNWRseIGpTBpRJelgEXatBLv3vGDMqCClhmfXQW1K6l3jXURdzOkCSKeL7/E4Z3bLN+9Kx6Q4i6v51wY9q2s1PsYeRUvRkPsR9LeMAtvnqbIiJRzNkPsMZwIDAQAB";
+    @Value("${aliface.aliPubKey}")
+    private String aliPubKey;
 
-    final String appPrivKey = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC1LKxhVY0wJtbebHxxt6xr8ujrpT9MXFTIy2QAy7s/QZgDgOp0fUnufSkGnAOis8ybD3OGCDNCN4HLhEhIF0TXnMYGigff97A5LS9qtrIYzroCX6q8nY9tnkNqSGjUg/sqTMhnYus6kABLu6f+zhQmivqIDB654XZEy4gc7tNQK/E4086ZVORj5a5lsE52L9mQxvRZNptdJu6FxnutpAFaQY51PQFsbTybGFY9jHiUozmmc6UFY5eo0vh21I7xyiJ4ObV8AoWxFfJd/IogK1sGPnhfWFaWZlcPpKVxMI0SmHLBVvVE+cGozYhzQ9pO8eb0/SypCOlE4unCDeqWSmttAgMBAAECggEAaXmqmbf+zIuwCCnMgT/XR7lRVO8NXN1cFtIiVDxHtNnxMAj7dfoC8q83FTv6+dwHQDbMoLlhbil2jQ9kLWQulgAVYXU2hHNkysS9mDjokny32e3sLobkG4NJ6DRjo2V68fNlsJrsK1nucii78yHypUW/+ThlIv5Zpk9/3KhFqaQPvL68mIt/AxttpvxkIGxTpfDsIiB55nXuA+my1txanBQGOph/bJjoVEonXaI5WXeDqyvrksMeBL1h4uUyQxbEhqwrdWrI+nGcWD1RSEFsDWcLk59r919BKvLudeq99fgRmIs4JjsX6NDQlJjgrgcQXlmuDPKVxWK4PbsAPxKzAQKBgQDrOn7AYK0iHzEL7XMLURmtc9iyYnYQZtBewfikGAa8mqHwRpXQvkF6iRWghxOUtsOBppDilI9mYSf9w48g9RJAuXW8Xhfh/OTFjuNVtddI9bZ2Kz6YmDCvpA/6vFBE6qLDiBu1DAc50A+QV03ftuWl/fYsKre314cnm65DWLaloQKBgQDFLD/6bsLlSejKVe0FVIHjePhICSjIpP3RCZKr3796V6ISUnbd+7gGGSIL02ugD6B6uPDRZg/c5n1E2n0SW7hTxdHEbArGqpR1iudh0NH+xoenM+futpG07p6Z34BLXzheLiexvXDz9AFWjuaa72HkoXO/zuVS7AUZW+Go3SBaTQKBgHdIDVhebc0cxhFHUoxNjtjPIvaqSTZd/JHgiMYlTN09CJtAH95fa/LIKwhhFsPVveQz48qSDPmckNv029KZk6zwnsxrZm75f8TShWNEN6xOtTzNBwv4ONAIQPYjogygcvL2RR6Uv+FIKWxHaTzlkSSQWNHiw5DdITMrlb2e8dehAoGAIzKB/T+WdP3bm3r5Q7FROn0WvS09ZYvmb1FpKFtZwAoyuViVNY1AzX3hEQFdvgXsHNw0yPySKlnO1OK33vYWkoXGq2yqA8rhk5YHNfPj+LoR7ywZ5AgdPJI4J7m5GjtWQaG1nkn2BuZw8rWxvsiqzfhwRDY3w7S0OFGpl85ZfyUCgYEAoHzVnhvN16iq4TFHFfH/oE7ZwKQvKcPQq5hMF7sj4CYux7PPPIrVFgkp5SYK1Wfp8tNcAV1HWh5VHxVfF52eg5R8ukg+K9XHOwRMmA14YZh54tjlzgztAFrtK/Or3bGilrb6980txGYiDraMKKLTGqrMFcLLaYBJ59JQlfF0jUA=";
+    @Value("${aliface.appPrivKey}")
+    private String appPrivKey;
 
     @PostMapping("/regist")
     public Object regist(HttpServletRequest request) throws AlipayApiException {
         Map requestParams = request.getParameterMap();
-        Map<String, String> sortedParams = new TreeMap<String, String>();
-        String sign = "";
-        for(Object key : requestParams.keySet()) {
-            String[] values = (String[])requestParams.get(key);
+        Map<String, String> sortedParams = new TreeMap<>();
+
+        for (Object key : requestParams.keySet()) {
+            String[] values = (String[]) requestParams.get(key);
             String valueStr = "";
             for (int i = 0; i < values.length; i++) {
                 valueStr = (i == values.length - 1) ? valueStr + values[i]
@@ -52,8 +55,6 @@ public class IdentityInfoQuery {
 
         System.out.println(verify_result);
 
-
-
         JsonRootBean jsonBean = new JsonRootBean();
         Institution institution = new Institution();
         institution.setId("2088200144875724");
@@ -68,7 +69,7 @@ public class IdentityInfoQuery {
         faceUser.setAlipay_uid_selector("0");
         faceUser.setExt_info("{\"key\":\"value\"}");
         List<Institution> institutions = new ArrayList();
-        institutions.add( institution);
+        institutions.add(institution);
         faceUser.setInstitution_list(institutions);
 
         Response response = new Response();
@@ -79,7 +80,7 @@ public class IdentityInfoQuery {
 
         System.out.println(JSON.toJSON(response));
 
-        String responseSign = AlipaySignature.sign(JSON.toJSON(response).toString(), appPrivKey,  StandardCharsets.UTF_8.name(), AlipayConstants.SIGN_TYPE_RSA2);
+        String responseSign = AlipaySignature.sign(JSON.toJSON(response).toString(), appPrivKey, StandardCharsets.UTF_8.name(), AlipayConstants.SIGN_TYPE_RSA2);
 
         jsonBean.setSign(responseSign);
         jsonBean.setResponse(response);
--- src/main/java/com/gmcc/aliface/response/JsonRootBean.java
+++ src/main/java/com/gmcc/aliface/response/JsonRootBean.java
@@ -4,18 +4,21 @@ public class JsonRootBean {
 
     private Response response;
     private String sign;
+
     public void setResponse(Response response) {
-         this.response = response;
-     }
-     public Response getResponse() {
-         return response;
-     }
+        this.response = response;
+    }
+
+    public Response getResponse() {
+        return response;
+    }
 
     public void setSign(String sign) {
-         this.sign = sign;
-     }
-     public String getSign() {
-         return sign;
-     }
+        this.sign = sign;
+    }
+
+    public String getSign() {
+        return sign;
+    }
 
 }

a1778f21a87e9c56af721428d0e1d6f6ae11c60b
jibenwujie58 <cr0508@foxmail.com>
2021-07-22 13:35:15 +0800

    method多接口

--- src/main/java/com/gmcc/aliface/controller/AlipayFaceBackend.java
+++ src/main/java/com/gmcc/aliface/controller/AlipayFaceBackend.java
@@ -1,32 +1,18 @@
 package com.gmcc.aliface.controller;
 
-import com.alibaba.fastjson.JSON;
 import com.alipay.api.AlipayApiException;
-import com.alipay.api.AlipayConstants;
-import com.alipay.api.internal.util.AlipaySignature;
 import com.gmcc.aliface.Service.AlipayFaceService;
-import com.gmcc.aliface.response.FaceUser;
-import com.gmcc.aliface.response.Institution;
-import com.gmcc.aliface.response.JsonRootBean;
-import com.gmcc.aliface.response.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 @RestController
 @RequestMapping(value = "/api/v1/aliUserReigstor")
 public class AlipayFaceBackend {
-
 
     @Autowired
     private AlipayFaceService alipayFaceService;
@@ -35,16 +21,15 @@ public class AlipayFaceBackend {
     public Object regist(HttpServletRequest request, @RequestParam String method) throws AlipayApiException {
         Object returnObj = null;
         switch (method) {
-            case "alipay.trade.generateUniqueId":
+            case "spi.alipay.commerce.frservice.generateUniqueId":
                 returnObj = alipayFaceService.generateUniqueId(request);
                 break;
-            case "":
+            case "spi.alipay.commerce.frservice.identityinfo.query":
                 returnObj = alipayFaceService.alipayInfoQuery(request);
                 break;
-            case "1":
+            case "spi.alipay.commerce.frservice.checkin.notify":
                 break;
             default:
-                int a = 1;
                 break;
         }
 
